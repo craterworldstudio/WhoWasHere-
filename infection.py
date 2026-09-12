c43fa3162daab4c086ffe582ad0ae7a467a0b974
tori <70708786+craterworldstudio@users.noreply.github.com>
2026-09-12 20:37:03 +0530

    Duplicate Trace and Questions Bug

--- infection.py
+++ infection.py
@@ -8,7 +8,7 @@ class Infections:
         self.traces_type_list = TRACE_TYPES
         self.persistance_type_list = PERSISTENCE_TYPES
 
-        self.applied_traces = [ list(self.traces_type_list.keys())[random.randint(0, len(self.traces_type_list)-1)]  for i in range(random.randint(5, 9))]
+        self.applied_traces = random.sample(list(self.traces_type_list.keys()), random.randint(5, min(9, len(self.traces_type_list))))
         self.persistance = list(self.persistance_type_list.keys())[random.randint(0, len(self.persistance_type_list)-1)]
 
 
